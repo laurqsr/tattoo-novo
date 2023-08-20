3b28e8103b9b1c6f17f3774b4544e42480638e67
laurqsr <santosribeiro.laura@gmail.com>
2023-08-20 20:19:55 -0300

    animaçãobv

---
+++ script.js
@@ -0,0 +1,10 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml10 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml10 .letter',
+    rotateY: [-90, 0],
+    duration: 2000,
+    delay: (el, i) => 100 * i
+  });
