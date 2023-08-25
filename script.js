c36b96cf805d1f5fa1bf90ccde545607edbae671
laurqsr <santosribeiro.laura@gmail.com>
2023-08-25 13:17:13 -0300

    aaaaaaa

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //bem vindo
 var textWrapper = document.querySelector('.ml10 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-anime.timeline({loop: true})
+anime.timeline({loop: true})//biblioteca, animação em loop
   .add({
-    targets: '.ml10 .letter',
-    rotateY: [-90, 0],
-    duration: 2000,
-    delay: (el, i) => 100 * i
+    targets: '.ml10 .letter', //os alvos da animação, no caso o h1 e o span
+    rotateY: [-90, 0], //gira as letras de -90 graus para 0 grau
+    duration: 2000, //duração da animação em milissegundos
+    delay: (el, i) => 100 * i //atraso para cada elemento
   });
   
 //menu
