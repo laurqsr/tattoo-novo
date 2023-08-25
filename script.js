ae3e0ea911d1db6bc33441fcc860ea48c38ba637
laurqsr <santosribeiro.laura@gmail.com>
2023-08-25 07:46:33 -0300

    metas

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ anime.timeline({loop: true})
     delay: (el, i) => 100 * i
   });
   
-
-  function myFunction() {
-    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
-  }
+//menu
+function myFunction() {
+  document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+}
